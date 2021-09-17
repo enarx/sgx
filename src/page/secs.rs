200e161b454a71251c333ebbcb2651a39bee01e0
Nathaniel McCallum <nathaniel@profian.com>
2021-09-17 13:42:28 -0400

    Clean up the Secs type
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/page/secs.rs
+++ src/page/secs.rs
@@ -1,16 +1,38 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! SECS (Section 38.7)
-//! The SGX Enclave Control Structure (SECS) is a special enclave page that is not
-//! visible in the address space. In fact, this structure defines the address
-//! range and other global attributes for the enclave and it is the first EPC
-//! page created for any enclave. It is moved from a temporary buffer to an EPC
-//! by the means of ENCLS(ECREATE) leaf.
-
 use crate::parameters::{Attributes, MiscSelect, Parameters};
-use core::num::{NonZeroU32, NonZeroUsize};
+use core::num::NonZeroU32;
 
-/// Section 38.7
+impl Parameters {
+    /// Creates a new `Secs` instance
+    ///
+    /// This creates a new instances of an SGX Enclave Control Structure (SECS)
+    /// page using the specified `Parameters` along with the `base` address and
+    /// `size` as well as the number of pages in an SSA frame.
+    pub fn secs(&self, base: *const (), size: usize, ssa_frame_pages: NonZeroU32) -> Secs {
+        Secs {
+            size: size as u64,
+            baseaddr: base as u64,
+            ssaframesize: ssa_frame_pages,
+            miscselect: self.misc.data & self.misc.mask,
+            reserved0: [0; 24],
+            attributes: self.attr.data & self.attr.mask,
+            mrenclave: [0; 32],
+            reserved1: [0; 32],
+            mrsigner: [0; 32],
+            reserved2: [0; 12],
+            isv_prod_id: self.isv_prod_id,
+            isv_svn: self.isv_svn,
+            reserved3: [0; 7],
+            reserved4: [[0; 28]; 17],
+        }
+    }
+}
+
+/// SGX Enclave Control Structure (SECS) page
+///
+/// The contents of `Secs` are entirely opaque as this type is passed directly
+/// to the hardware during enclave creation and never user readable.
 #[derive(Copy, Clone, Debug)]
 #[repr(C, align(4096))]
 pub struct Secs {
@@ -28,82 +50,6 @@ pub struct Secs {
     isv_svn: u16,
     reserved3: [u32; 7],
     reserved4: [[u64; 28]; 17],
-}
-
-impl Secs {
-    /// Creates a new SECS struct based on a base address and spec.
-    pub fn new(
-        addr: *const (),
-        size: usize,
-        ssa_frame_pages: NonZeroU32,
-        parameters: Parameters,
-    ) -> Self {
-        Self {
-            size: size as u64,
-            baseaddr: addr as u64,
-            ssaframesize: ssa_frame_pages,
-            miscselect: parameters.misc.data & parameters.misc.mask,
-            reserved0: [0; 24],
-            attributes: parameters.attr.data & parameters.attr.mask,
-            mrenclave: [0; 32],
-            reserved1: [0; 32],
-            mrsigner: [0; 32],
-            reserved2: [0; 12],
-            isv_prod_id: parameters.isv_prod_id,
-            isv_svn: parameters.isv_svn,
-            reserved3: [0; 7],
-            reserved4: [[0; 28]; 17],
-        }
-    }
-
-    /// # Usage
-    /// Returns the maximum enclave size for 64bit in bytes.
-    /// CPUID.(EAX=12H, ECX=0H) enumerates Intel SGX capability;
-    /// For more on CPUID enumeration leaves, see 37.7.2 and Table 37-4.
-    ///
-    /// # Safety
-    /// This function is technically unsafe because it does not check if the
-    /// `CPUID` instruction is available before issuing it. This could result
-    /// in a crash on some very old CPUs. However, the only modern context
-    /// where it could crash is environments like SGX or some virtualized
-    /// CPUs. But even in these contexts, it is common to trap and emulate the
-    /// instruction.
-    ///
-    /// Therefore, it is common practice to ignore the test to see if the
-    /// `CPUID` instruction is available and just issue it anyway. For this
-    /// reason, we are marking this function as safe. For more background to
-    /// the state of `CPUID` in Rust, see:
-    ///
-    /// https://github.com/rust-lang/rust/issues/60123
-    ///
-    pub fn max_enc_size() -> Option<NonZeroUsize> {
-        use core::arch::x86_64::{__cpuid_count, __get_cpuid_max};
-
-        const LEAF_MAX_PARAM: u32 = 0x0;
-        const LEAF_SGX_SUPPORT: u32 = 0x07;
-        const SUBLEAF_SGX_SUPPORT: u32 = 0x0;
-        const LEAF_MAX_ENCL_SIZE: u32 = 0x12;
-        const SUBLEAF_MAX_ENCL_SIZE: u32 = 0x0;
-
-        // Test for max leaf size
-        let res = unsafe { __get_cpuid_max(LEAF_MAX_PARAM) };
-        let max_leaf = res.0;
-        if max_leaf < LEAF_SGX_SUPPORT || max_leaf < LEAF_MAX_ENCL_SIZE {
-            return None;
-        }
-
-        // Test for SGX support
-        let res = unsafe { __cpuid_count(LEAF_SGX_SUPPORT, SUBLEAF_SGX_SUPPORT) };
-        if res.ebx & (1 << 2) == 0 {
-            return None;
-        }
-
-        // Test for max enclave size
-        let res = unsafe { __cpuid_count(LEAF_MAX_ENCL_SIZE, SUBLEAF_MAX_ENCL_SIZE) };
-        let max_size: u64 = 1 << (res.edx >> 8u8) as u64;
-
-        NonZeroUsize::new(max_size as usize)
-    }
 }
 
 #[cfg(test)]
