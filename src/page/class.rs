2aefb0570a9a8bca8cd271f9f0bc946eb86b98b7
Nathaniel McCallum <nathaniel@profian.com>
2021-09-17 14:12:49 -0400

    Clean up Class documentation
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/page/class.rs
+++ src/page/class.rs
@@ -1,23 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
-/// The `Class` of a page
+/// The class of a page
 ///
-/// The `Class` type is the `PAGE_TYPE` data structure, merely renamed
-/// due to the collision with the Rust `type` keyword.
-///
-/// Section 38.11.2
+/// This enumeration identifies the kind of one or more pages. This type is
+/// named `PAGE_TYPE` in the Intel documentation.
 #[repr(u8)]
 #[non_exhaustive]
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Class {
-    /// Page is an SECS.
     Secs = 0,
-    /// Page is a TCS.
     Tcs = 1,
-    /// Page is a regular page.
     Reg = 2,
-    /// Page is a Version Array.
     Va = 3,
-    /// Page is in trimmed state.
     Trim = 4,
 }
