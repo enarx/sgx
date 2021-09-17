46af25b5884361c322d6537b713a0a4e1f2653fb
Nathaniel McCallum <nathaniel@profian.com>
2021-09-17 16:11:30 -0400

    Enable std during test
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! Section references in further documentation refer to this document.
 //! https://www.intel.com/content/dam/www/public/emea/xe/en/documents/manuals/64-ia-32-architectures-software-developer-vol-3d-part-4-manual.pdf
 
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 #![deny(clippy::exhaustive_enums)]
 #![deny(clippy::all)]
 
