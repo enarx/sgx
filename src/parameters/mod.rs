17c24be15a68f45569a11dd36fb47f513bab9739
Nathaniel McCallum <nathaniel@profian.com>
2021-09-22 08:08:55 -0400

    Make Parameters repr(C)
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/parameters/mod.rs
+++ src/parameters/mod.rs
@@ -39,6 +39,7 @@ bitflags::bitflags! {
 /// contexts. For example, when creating an `Secs` page, the mask represents
 /// the platform-supported features. Likewise, when creating a `Signature`
 /// the mask represents the required features for the enclave.
+#[repr(C)]
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
 pub struct Parameters {
     /// Choose info for the `Misc` section of the `StateSaveArea`
