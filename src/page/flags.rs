439143f8e5485d6d42b457088bc85192cf91f09b
Nathaniel McCallum <nathaniel@profian.com>
2021-12-03 20:48:32 +0000

    impl Display for SecInfo
    This provides us with some short output helpful for trace logging.
    
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/page/flags.rs
+++ src/page/flags.rs
@@ -15,3 +15,44 @@ bitflags::bitflags! {
         const RESTRICTED = 1 << 5;
     }
 }
+
+impl core::fmt::Display for Flags {
+    #[inline]
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let opts = [
+            (Self::READ, 'R'),
+            (Self::WRITE, 'W'),
+            (Self::EXECUTE, 'X'),
+            (Self::PENDING, 'P'),
+            (Self::MODIFIED, 'M'),
+            (Self::RESTRICTED, '!'),
+        ];
+
+        for (flag, val) in opts {
+            if self.contains(flag) {
+                write!(f, "{}", val)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn display() {
+        assert_eq!(format!("{}", Flags::READ), "R");
+        assert_eq!(format!("{}", Flags::WRITE), "W");
+        assert_eq!(format!("{}", Flags::EXECUTE), "X");
+        assert_eq!(format!("{}", Flags::READ | Flags::WRITE), "RW");
+        assert_eq!(format!("{}", Flags::READ | Flags::EXECUTE), "RX");
+        assert_eq!(format!("{}", Flags::WRITE | Flags::EXECUTE), "WX");
+        assert_eq!(
+            format!("{}", Flags::READ | Flags::WRITE | Flags::EXECUTE),
+            "RWX"
+        );
+    }
+}
