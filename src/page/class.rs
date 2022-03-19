222c9366eeb194c62d02dded98b6fca29b284ac4
Jarkko Sakkinen <jarkko@profian.com>
2022-03-19 15:09:24 +0200

    refactor: SecInfo and Class
    Based on https://github.com/rust-osdev/x86_64/pull/348.
    
    Closes: #72
    Signed-off-by: Jarkko Sakkinen <jarkko@profian.com>

--- src/page/class.rs
+++ src/page/class.rs
@@ -1,16 +1,33 @@
 // SPDX-License-Identifier: Apache-2.0
 
-/// The class of a page
-///
-/// This enumeration identifies the kind of one or more pages. This type is
-/// named `PAGE_TYPE` in the Intel documentation.
+use crate::page::{Flags, SecInfo};
+
+/// The type of an enclave page (see Intel SDM Volume 3D section 34.12.2).
+/// Enclave Page Cache Map (EPCM) hols this information for each valid enclave
+/// page.
 #[repr(u8)]
 #[non_exhaustive]
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Class {
+    /// SGX Enclave Control Structure (SECS)
     Secs = 0,
+    /// Thread Control Structure (TCS)
     Tcs = 1,
-    Reg = 2,
-    Va = 3,
-    Trim = 4,
+    /// Regularular page
+    Regular = 2,
+    /// Version Array (VA) page
+    VersionArray = 3,
+    /// Removable from a running enclave
+    Trimmed = 4,
+    /// The first page of a shadow stack
+    ShadowStackFirst = 5,
+    /// A shadow stack page
+    ShadowStackRest = 6,
+}
+
+impl Class {
+    /// Convert to SecInfo with the given flags.
+    pub fn info(&self, flags: impl Into<Option<Flags>>) -> SecInfo {
+        SecInfo::new(*self, flags.into())
+    }
 }
